c0d1e850e6ef460ccbe1bd97a127b230ef67cb4c
Machadoerica <144906356+Machadoerica@users.noreply.github.com>
2024-03-21 08:48:11 -0300

    ok

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
+const contadores = document.querySelectorAll(".contador");
 
 for(let i=0; i <botoes.length; i++){
     botoes[i].onclick = function(){
@@ -15,5 +16,7 @@ for(let i=0; i <botoes.length; i++){
 
     }
 }
-contadores[0].textContent = "contagem regressiva";
-const tempoObjetivo1 = new Date(2024/12/12);
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-12T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1;
